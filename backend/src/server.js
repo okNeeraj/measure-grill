0e077ee1d46c3f17d8d38042fbc4b1caa1462509
Neeraj Singh <neerajonetwo@gmail.com>
2023-12-28 09:03:56 +0530

    Utils and express app setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,5 +4,19 @@ dotenv.config({
 });
 
 import connectDB from "./config/database.js";
+import { app } from "./app.js"
 
-connectDB();
+connectDB()
+	.then(() => {
+		app.on("Error", (error) => {
+			console.log("Application Error !!", error);
+			throw error;
+		})
+		app.listen(process.env.PORT || 8000, () => {
+			console.log(`Server is running at port ${process.env.PORT}`)
+		})
+	})
+	.catch((error) => {
+		console.log('Database Error !!', error)
+	})
+
